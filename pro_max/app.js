bdc2e7bec220e689f91cd1f2df038506fe5deac7
tawat-234 <gurutawat@gmail.com>
2026-07-18 10:28:37 +0700

    Add files via upload

--- pro_max/app.js
+++ pro_max/app.js
@@ -1,13 +1,23 @@
-
-
 const img = document.getElementById("img");
+
+
+img.classList.remove("fade-in");
+
+img.src = "wait.png";
+img.style.display = "block";
+
+
+let isSpeaking = false;
+let lastName = "";
+let clearTimer = null;
+
 
 const client = mqtt.connect("wss://broker.hivemq.com:8884/mqtt");
 
 client.on("connect", () => {
     console.log("✅ Connected to broker");
 
-    client.subscribe("test123", (err) => {
+    client.subscribe("poy0859156151", (err) => {
         if (err) {
             console.error("❌ Subscribe error:", err);
            
@@ -27,39 +37,101 @@ client.on("offline", () => {
 
 });
 
-client.on("message", (topic, message) => {
+async function sendToPython(word){
 
-    const raw = message.toString();
+    console.log("Sending:", word);
 
-    console.log("RAW:", raw);
-
-    let data;
-
-    // SAFE PARSING (prevents invalid JSON crash)
     try {
-        data = JSON.parse(raw);
-    } catch (e) {
-        console.log("⚠️ Not JSON, treating as text");
 
-        data = {
-            payload: raw
-        };
+        const response = await fetch("http://127.0.0.1:5000/speak", {
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                message:word
+            })
+        });
+
+        const data = await response.json();
+
+        console.log(data);
+
+
+        if(data.status === "finished"){
+
+
+          img.classList.remove("fade-in");
+          img.classList.add("fade-out");
+
+
+          await new Promise(resolve => setTimeout(resolve, 1000));
+
+
+          img.classList.remove("fade-out");
+
+          img.src = "wait.png";
+
+          img.onload = () => {img.classList.add("fade-in");};
+
+            console.log("Speech finished");
+
+            // รอ 5 วินาที
+            await new Promise(resolve => setTimeout(resolve, 5000));
+
+            console.log("Ready again");
+
+
+
+            isSpeaking = false;
+        }
+
+
+    } catch(error){
+
+        console.error(error);
+
+        isSpeaking = false;
+    }
+}
+
+
+client.on("message", async (topic, message) => {
+
+    const data = JSON.parse(message.toString());
+
+    const name = data?.DeepDetect?.obj_count?.[0]?.name;
+
+
+    if(isSpeaking) return;
+
+
+    if(name === "sardine"){
+
+     console.log("SARDINE DETECTED");
+
+     isSpeaking = true;
+
+     img.classList.remove("fade-out");
+     void img.offsetWidth;
+
+     img.classList.remove("fade-out");
+     img.classList.remove("fade-in");
+
+     void img.offsetWidth;
+
+     img.src = "Sardinenew.png";
+
+     img.onload = () => {img.classList.add("fade-in");};
+
+     // กรณีรูปถูก cache แล้ว
+     if (img.complete) {
+        img.classList.add("fade-in");
+     } 
+
+     console.log("Image changed:", img.src);
+
+     await sendToPython("sardine");
     }
 
-    console.log("PARSED DATA:", data);
-
-    // CHECK VALUE
-    if (data.payload === "Sardine") {
-
-        console.log("🐟 Sardine detected!");
-
-        img.src = "Sardine.png";
-        img.style.display = "block";
-
-    }
-    else if (data.payload === "Tofu"){
-     
-      img.src = "Tofu.png";
-      img.style.display = "block";
-    }
 });
